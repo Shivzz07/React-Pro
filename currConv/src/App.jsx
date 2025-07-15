5f47e5707a2fd7672e16feb6f68fff629c1bbb33
Shivzz <shivammittal344@gmail.com>
2025-07-15 10:43:04 +0000

    new project

---
+++ currConv/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <h1 className='text-amber-300 text-5xl'>Hello world</h1>
+    </>
+  )
+}
+
+
+export default App
